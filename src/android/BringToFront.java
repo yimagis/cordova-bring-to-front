41b87f685be17a40328a57475b166b25a02d3762
yimagis <35189612+yimagis@users.noreply.github.com>
2018-01-08 03:04:19 +0800

    Update BringToFront.java

--- src/android/BringToFront.java
+++ src/android/BringToFront.java
@@ -7,6 +7,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import android.content.Intent;
+import android.content.ComponentName;
 import android.app.PendingIntent;
 import android.util.Log;
 import android.app.Activity;
@@ -22,6 +23,17 @@ public class BringToFront extends CordovaPlugin {
     if (action.equals("bringToFront")) {
       Log.d("Bring", "I see you baby");
       
+          Intent it = new Intent("android.intent.action.MAIN");
+          String packageName = args.getString(0);
+
+          Activity activity = this.cordova.getActivity();
+          it.setComponent(new ComponentName("com.phonegap.helloworld", "com.phonegap.helloworld.CordovaApp"));
+	        it.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+          Context context = activity.getApplicationContext();
+          context.startActivity(it);
+
+      
      /* Intent newIntent = new Intent();
         newIntent.setClassName("com.phonegap.helloworld", "com.phonegap.helloworld.CordovaApp");
         newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP  | Intent.FLAG_ACTIVITY_SINGLE_TOP);
@@ -30,7 +42,7 @@ public class BringToFront extends CordovaPlugin {
       Context context = activity.getApplicationContext();      
         context.startActivity(newIntent); */
       
-      
+      /*
       Intent notificationIntent = new Intent(cordova.getActivity(), cordova.getActivity().getClass());
       notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
       PendingIntent pendingIntent = PendingIntent.getActivity(cordova.getActivity(), 0, notificationIntent, 0);
@@ -41,7 +53,7 @@ public class BringToFront extends CordovaPlugin {
       catch (PendingIntent.CanceledException e) 
       {
         e.printStackTrace();
-      }
+      }*/
       return true;
     }
     return false;
