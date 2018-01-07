2bc59d4d4069978ea98e18bc80448d8d0ded52fb
yimagis <35189612+yimagis@users.noreply.github.com>
2018-01-08 02:57:11 +0800

    Update BringToFront.java

--- src/android/BringToFront.java
+++ src/android/BringToFront.java
@@ -22,15 +22,15 @@ public class BringToFront extends CordovaPlugin {
     if (action.equals("bringToFront")) {
       Log.d("Bring", "I see you baby");
       
-      Intent newIntent = new Intent();
+     /* Intent newIntent = new Intent();
         newIntent.setClassName("com.phonegap.helloworld", "com.phonegap.helloworld.CordovaApp");
         newIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP  | Intent.FLAG_ACTIVITY_SINGLE_TOP);
       
        Activity activity = this.cordova.getActivity();
       Context context = activity.getApplicationContext();      
-        context.startActivity(newIntent); 
+        context.startActivity(newIntent); */
       
-      /*
+      
       Intent notificationIntent = new Intent(cordova.getActivity(), cordova.getActivity().getClass());
       notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
       PendingIntent pendingIntent = PendingIntent.getActivity(cordova.getActivity(), 0, notificationIntent, 0);
@@ -41,7 +41,7 @@ public class BringToFront extends CordovaPlugin {
       catch (PendingIntent.CanceledException e) 
       {
         e.printStackTrace();
-      }*/
+      }
       return true;
     }
     return false;
